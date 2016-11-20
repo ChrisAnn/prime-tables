325b4d562b861ec8bfbdf0b40b7732eb34a5ad56
ChrisAnn <christine.ann.odell@gmail.com>
2016-11-20 17:11:14 +0000

    Added basic test for trapping console output

---
+++ src/PrimeTables.Test/ProgramTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace PrimeTables.Test
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void ValidateConsoleOutput()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                Program.Main(null);
+
+                string expected = $"Hello, world!{Environment.NewLine}";
+                Assert.That(sw.ToString(), Is.EqualTo(expected));
+            }
+        }
+    }
+}
