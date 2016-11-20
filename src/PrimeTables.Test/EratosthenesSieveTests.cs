7e69252081d10e0c48a05d958372220f2641bfe1
ChrisAnn <christine.ann.odell@gmail.com>
2016-11-20 18:23:24 +0000

    Created Class to contain the Eratosthenes Sieve algorithm

---
+++ src/PrimeTables.Test/EratosthenesSieveTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace PrimeTables.Test
+{
+    [TestFixture]
+    public class EratosthenesSieveTests
+    {
+        [TestCase (2, new[] {2})]
+        public void OutputsExpectedPrimes(int max, IEnumerable expectedPrimes)
+        {
+            Assert.That(EratosthenesSieve.SievePrimes(max), Is.EquivalentTo(expectedPrimes));
+        }
+    }
+}
---
+++ src/PrimeTables/EratosthenesSieve.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeTables
+{
+    public class EratosthenesSieve
+    {
+        public static IList<int> SievePrimes(int max)
+        {
+            return new List<int> {2};
+        }
+    }
+}
