aefbfa562ead6eed3b1f35e83599a89fcacc059b
ChrisAnn <christine.ann.odell@gmail.com>
2016-11-20 21:55:48 +0000

    Estimate Prime at the given size, sieve primes below it, output grid
    Added test for grid size of 10.

--- src/PrimeTables/Program.cs
+++ src/PrimeTables/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrimeTables
 {
@@ -9,21 +11,41 @@ namespace PrimeTables
             Console.WriteLine("Enter Table Size:");
             var readLine = Console.ReadLine();
 
-            int tableSize;
-            if (!int.TryParse(readLine, out tableSize) || tableSize < 1)
+            int size;
+            if (!int.TryParse(readLine, out size) || size < 1)
             {
                 Console.WriteLine("Not a valid integer.");
                 return;
             }
 
-            Console.WriteLine($"Table Size: {tableSize}");
+            Console.WriteLine($"Table Size: {size}");
 
-            if (tableSize == 3)
+            var estimatedPrimeAtSize = EstimatePrimeAtN(size);
+            var primes = EratosthenesSieve.SievePrimes(estimatedPrimeAtSize);
+
+            PrintGrid(primes.Take(size).ToList());
+        }
+
+        private static int EstimatePrimeAtN(int n)
+        {
+            if (n >= 6)
             {
-                Console.Write(  "|    |  2 |  3 |  5 |\r\n" +
-                                "|  2 |  4 |  6 | 10 |\r\n" +
-                                "|  3 |  6 |  9 | 15 |\r\n" +
-                                "|  5 | 10 | 15 | 25 |\r\n");
+                return (int)Math.Ceiling(n * Math.Log(n) + n * Math.Log(Math.Log(n)));
+            }
+
+            return new[] { 2, 3, 5, 7, 11 }[n - 1];
+        }
+
+        private static void PrintGrid(IList<int> primes)
+        {
+            for (var row = 0; row <= primes.Count; row++)
+            {
+                var rowString = row == 0 ? "|    |" : $"|{primes[row-1], 3} |";
+                foreach (var column in primes)
+                {
+                    rowString += row == 0 ? $"{column, 3} |" : $"{primes[row - 1] * column, 3} |";
+                }
+                Console.WriteLine(rowString);
             }
         }
     }
