f3f7beb551daa7d20e054136e3d8514fb7265f0c
ChrisAnn <christine.ann.odell@gmail.com>
2016-11-20 19:01:42 +0000

    Added implementation of Eratosthenes Sieve

--- src/PrimeTables.Test/EratosthenesSieveTests.cs
+++ src/PrimeTables.Test/EratosthenesSieveTests.cs
@@ -7,6 +7,7 @@ namespace PrimeTables.Test
     public class EratosthenesSieveTests
     {
         [TestCase (2, new[] {2})]
+        [TestCase (5, new[] {2, 3, 5})]
         public void OutputsExpectedPrimes(int max, IEnumerable expectedPrimes)
         {
             Assert.That(EratosthenesSieve.SievePrimes(max), Is.EquivalentTo(expectedPrimes));
--- src/PrimeTables/EratosthenesSieve.cs
+++ src/PrimeTables/EratosthenesSieve.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrimeTables
 {
@@ -6,7 +8,35 @@ namespace PrimeTables
     {
         public static IList<int> SievePrimes(int max)
         {
-            return new List<int> {2};
+            var primes = new List<int>();
+            var sieve = CreateSieve(max);
+
+            for (var i = 2; i <= max; i++)
+            {
+                if (sieve[i])
+                    primes.Add(i);
+            }
+            
+            return primes;
+        }
+
+        private static bool[] CreateSieve(int max)
+        {
+            var isPrime = Enumerable.Repeat(true, max + 1).ToArray();
+
+            var squareRootMax = Math.Ceiling(Math.Sqrt(max));
+
+            for (var i = 2; i <= squareRootMax; i++)
+            {
+                if (!isPrime[i]) continue;
+
+                for (var j = i*2; j < max; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+
+            return isPrime;
         }
     }
 }
